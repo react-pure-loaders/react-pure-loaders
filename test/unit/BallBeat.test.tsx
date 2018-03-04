392594f7a46d1b709a11b7f77d3de2f8110f49bd
James W. Lane <james.w.lane@mac.com>
2018-03-04 08:40:26 -0600

    test(BallBeat): Improved test and coverage

--- test/unit/BallBeat.test.tsx
+++ test/unit/BallBeat.test.tsx
@@ -1,28 +1,45 @@
-import { mount, render, shallow } from 'enzyme';
-import { StyleSheet } from 'glamor/lib/sheet';
+import * as Chance from 'chance';
+import { shallow } from 'enzyme';
 import * as React from 'react';
-import { BallBeat } from '../../src';
 
-const stylesheet = new StyleSheet();
+import { BallBeat } from '../../src';
+import { getClassName, getCssValueByProperty } from '../utilities';
+
+const chance = new Chance();
 
 describe('<BallBeat>', () => {
-  beforeEach(() => {
-    stylesheet.flush();
-  });
-
-  test('shallow to match snapshot', () => {
+  test('BallBeat should match snapshot', () => {
     const ui = (<BallBeat loading={true}/>);
 
-    expect(shallow(ui)).toMatchSnapshot(`enzyme.shallow`);
+    expect(shallow(ui)).toMatchSnapshot();
   });
-  test('mount to match snapshot', () => {
+
+  test('BallBeat should have three children', () => {
     const ui = (<BallBeat loading={true}/>);
 
-    expect(mount(ui)).toMatchSnapshot(`enzyme.mount`);
+    expect(shallow(ui).children()).toHaveLength(3);
   });
-  test('render to match snapshot', () => {
-    const ui = (<BallBeat loading={true}/>);
 
-    expect(render(ui)).toMatchSnapshot(`enzyme.render`);
+  test('BallBeat should have no children', () => {
+    const ui = (<BallBeat loading={false}/>);
+
+    expect(shallow(ui).children()).toHaveLength(0);
+  });
+
+  test('BallBeat should have default color', () => {
+    const ui = (<BallBeat loading={true}/>);
+    const className = getClassName(ui);
+
+    const currentDisplayValue = getCssValueByProperty(className,'> div','background-color');
+    expect(currentDisplayValue).toEqual('#ffffff');
+  });
+
+  test('BallBeat should have given color', () => {
+    const color = chance.color();
+    const ui = (<BallBeat color={color} loading={true}/>);
+    const className = getClassName(ui);
+
+    const currentDisplayValue = getCssValueByProperty(className,'> div','background-color');
+    expect(currentDisplayValue).toEqual(color);
   });
 });
