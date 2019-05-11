3bd9fd568eeeacf6b10fd89c9a6beaa2228fb2f4
James W. Lane <james.w.lane@mac.com>
2019-05-11 17:00:45 -0500

    fix(Exports): Fixed exports if pulling in all loaders

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,43 @@
 /**
  * Dots
  */
-export * from './BallBeat';
-export * from './BallClipRotate';
-export * from './BallClipRotateMultiple';
-export * from './BallClipRotatePulse';
-export * from './BallGridBeat';
-export * from './BallGridPulse';
-export * from './BallPulse';
-export * from './BallPulseRise';
-export * from './BallPulseRound';
-export * from './BallPulseSync';
-export * from './BallRotate';
-export * from './BallScale';
-export * from './BallScaleMultiple';
-export * from './BallScaleRandom';
-export * from './BallScaleRipple';
-export * from './BallScaleRippleMultiple';
-export * from './BallSpinFadeLoader';
-export * from './BallSpinLoader';
-export * from './BallTrianglePath';
-export * from './BallZigZag';
-export * from './BallZigZagDeflect';
+export { default as BallBeat } from './BallBeat';
+export { default as BallClipRotate } from './BallClipRotate';
+export { default as BallClipRotateMultiple } from './BallClipRotateMultiple';
+export { default as BallClipRotatePulse } from './BallClipRotatePulse';
+export { default as BallGridBeat } from './BallGridBeat';
+export { default as BallGridPulse } from './BallGridPulse';
+export { default as BallPulse } from './BallPulse';
+export { default as BallPulseRise } from './BallPulseRise';
+export { default as BallPulseRound } from './BallPulseRound';
+export { default as BallPulseSync } from './BallPulseSync';
+export { default as BallRotate } from './BallRotate';
+export { default as BallScale } from './BallScale';
+export { default as BallScaleMultiple } from './BallScaleMultiple';
+export { default as BallScaleRandom } from './BallScaleRandom';
+export { default as BallScaleRipple } from './BallScaleRipple';
+export { default as BallScaleRippleMultiple } from './BallScaleRippleMultiple';
+export { default as BallSpinFadeLoader } from './BallSpinFadeLoader';
+export { default as BallSpinLoader } from './BallSpinLoader';
+export { default as BallTrianglePath } from './BallTrianglePath';
+export { default as BallZigZag } from './BallZigZag';
+export { default as BallZigZagDeflect } from './BallZigZagDeflect';
 
 /**
  * Lines
  */
-export * from './LineScale';
-export * from './LineScaleParty';
-export * from './LineScalePulseOut';
-export * from './LineScalePulseOutRapid';
-export * from './LineScaleRandom';
-export * from './LineSpinFadeLoader';
+export { default as LineScale } from './LineScale';
+export { default as LineScaleParty } from './LineScaleParty';
+export { default as LineScalePulseOut } from './LineScalePulseOut';
+export { default as LineScalePulseOutRapid } from './LineScalePulseOutRapid';
+export { default as LineScaleRandom } from './LineScaleRandom';
+export { default as LineSpinFadeLoader } from './LineSpinFadeLoader';
 
 /**
  * Misc
  */
-export * from './CubeTransition';
-export * from './Pacman';
-export * from './SemiCircleSpin';
-export * from './SquareSpin';
-export * from './TriangleSkewSpin';
+export { default as CubeTransition } from './CubeTransition';
+export { default as Pacman } from './Pacman';
+export { default as SemiCircleSpin } from './SemiCircleSpin';
+export { default as SquareSpin } from './SquareSpin';
+export { default as TriangleSkewSpin } from './TriangleSkewSpin';
