35f9137dd0d12281d71d35d531d97147650172f7
Willane Paiva <willane.paiva@codeminer42.com>
2019-06-19 11:06:41 -0300

    Generate new build after knobs

---
+++ storybook-static/main.df21d7ad8a075a3abb9a.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{450:function(n,o,p){p(451),p(554),n.exports=p(1030)},554:function(n,o,p){"use strict";p(555),p(928),p(990),p(993)}},[[450,1,2]]]);
