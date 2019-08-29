d9585a8eae619301b654d32d0441aadcf3172e17
Larissagilliane <larissagilliane@hotmail.com>
2019-08-29 16:21:50 -0300

    BallGridPulse animation correction

--- src/BallGridPulse/style.ts
+++ src/BallGridPulse/style.ts
@@ -1,21 +1,21 @@
-// TODO
-// import { keyframes } from '@emotion/core';
+import { keyframes } from '@emotion/core';
 import { ballGrid, balls, globalAnimation } from '../mixins';
 import { BALL_SIZE, MARGIN } from '../variables';
 
-// const ballGridPulse = keyframes({
-//     '0%': {
-//         transform: 'scale(1)',
-//     },
-//     '100%': {
-//         opacity: 1,
-//         transform: 'scale(1)',
-//     },
-//     '50%': {
-//         opacity: 0.7,
-//         transform: 'scale(0.5)',
-//     },
-// });
+const ballGridPulse = keyframes({
+    '0%': {
+        transform: 'scale(1)',
+    },
+    '100%': {
+        opacity: 1,
+        transform: 'scale(1)',
+    },
+    '50%': {
+        opacity: 0.7,
+        transform: 'scale(0.5)',
+    },
+});
+
 
 export default (color?: string) => ({
     width: (parseInt(BALL_SIZE, 10) * 3) + parseInt(MARGIN, 10) * 6,
@@ -24,7 +24,7 @@ export default (color?: string) => ({
         ...globalAnimation(),
         animationDelay: '0',
         animationIterationCount: 'infinite',
-        // animationName: ballGridPulse,
+        animationName: ballGridPulse,
         display: 'inline-block',
         float: 'left' as 'left',
     },
