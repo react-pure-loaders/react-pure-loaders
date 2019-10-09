83732d8cb56cf5a238123bd0fc691af4cef1be9d
Larissagilliane <larissagilliane@hotmail.com>
2019-10-09 15:02:46 -0300

    correction ballPulseRise

--- src/BallPulseRise/style.ts
+++ src/BallPulseRise/style.ts
@@ -1,67 +1,66 @@
-// TODO
-// import { keyframes } from '@emotion/core';
+import { keyframes } from '@emotion/core';
 import { balls, globalAnimation } from '../mixins';
 
-// const riseAmount = '30px';
+const riseAmount = '30px';
 
-// const ballPulseRiseEven = keyframes({
-//     '0%': {
-//         transform: 'scale(1.1)',
-//     },
-//     '100%': {
-//         transform: 'scale(1.0)',
-//     },
-//     '25%': {
-//         transform: `translateY(-${riseAmount})`,
-//     },
-//     '50%': {
-//         transform: 'scale(0.4)',
-//     },
-//     '75%': {
-//         transform: `translateY(${riseAmount})`,
-//     },
-//     '99%': {
-//         transform: 'translateY(0)',
-//     },
-// });
+const ballPulseRiseEven = keyframes({
+    '0%': {
+        transform: 'scale(1.1)',
+    },
+    '100%': {
+        transform: 'scale(1.0)',
+    },
+    '25%': {
+        transform: `translateY(-${riseAmount})`,
+    },
+    '50%': {
+        transform: 'scale(0.4)',
+    },
+    '75%': {
+        transform: `translateY(${riseAmount})`,
+    },
+    '99%': {
+        transform: 'translateY(0)',
+    },
+});
 
-// const ballPulseRiseOdd = keyframes({
-//     '0%': {
-//         transform: 'scale(0.4)',
-//     },
-//     '100%': {
-//         transform: 'scale(0.75)',
-//     },
-//     '25%': {
-//         transform: `translateY(${riseAmount})`,
-//     },
-//     '50%': {
-//         transform: 'scale(1.1)',
-//     },
-//     '75%': {
-//         transform: `translateY(-${riseAmount})`,
-//     },
-//     '99%': {
-//         transform: 'translateY(0)',
-//     },
-// });
+const ballPulseRiseOdd = keyframes({
+    '0%': {
+        transform: 'scale(0.4)',
+    },
+    '100%': {
+        transform: 'scale(0.75)',
+    },
+    '25%': {
+        transform: `translateY(${riseAmount})`,
+    },
+    '50%': {
+        transform: 'scale(1.1)',
+    },
+    '75%': {
+        transform: `translateY(-${riseAmount})`,
+    },
+    '99%': {
+        transform: 'translateY(0)',
+    },
+});
 
 export default (color?: string) => ({
-  ' > div': {
-    ...balls(color),
-    ...globalAnimation(),
-    animationDelay: '0',
-    animationDuration: '1s',
-    animationIterationCount: 'infinite',
-    animationTimingFunction: 'cubic-bezier(.15,.46,.9,.6)',
-    display: 'inline-block',
+    ' > div': {
+        ...balls(color),
+        ...globalAnimation(),
+        animationDelay: '0',
+        animationDuration: '1s',
+        animationIterationCount: 'infinite',
+        animationTimingFunction: 'cubic-bezier(.15,.46,.9,.6)',
+        display: 'inline-block',
 
-    '&:nth-of-type(2n)': {
-      // animationName: ballPulseRiseEven,
-    },
+        '&:nth-of-type(2n)': {
+            animationName: ballPulseRiseEven,
+        },
 
-    '&:nth-of-type(2n-1)': {
-      // animationName: ballPulseRiseOdd,
+        '&:nth-of-type(2n-1)': {
+            animationName: ballPulseRiseOdd,
+        },
     },
-  },
 });
--- test/unit/BallPulseRise.test.tsx
+++ test/unit/BallPulseRise.test.tsx
@@ -15,32 +15,40 @@ describe('<BallPulseRise>', () => {
     afterEach(cleanup);
 
     test('BallPulseRise should match snapshot', () => {
-        const { container } = render(<BallPulseRise loading={true}/>);
+        const { container } = render(<BallPulseRise loading={true} />);
 
         expect(container.firstChild).toMatchSnapshot();
     });
 
     test('BallPulseRise should have default color', () => {
-        const { container } = render(<BallPulseRise loading={true}/>);
+        const { container } = render(<BallPulseRise loading={true} />);
 
-        expect(container.firstChild).toHaveStyleRule('background-color', PRIMARY_COLOR, { target: '> div' });
+        expect(container.firstChild).toHaveStyleRule(
+            'background-color',
+            PRIMARY_COLOR,
+            { target: '> div' },
+        );
     });
 
     test('BallPulseRise should have given color', () => {
         const color = chance.color({ format: 'hex' });
-        const { container } = render(<BallPulseRise color={color} loading={true}/>);
+        const { container } = render(
+            <BallPulseRise color={color} loading={true} />,
+        );
 
-        expect(container.firstChild).toHaveStyleRule('background-color', color, { target: '> div' });
+        expect(container.firstChild).toHaveStyleRule('background-color', color, {
+            target: '> div',
+        });
     });
 
     test('BallPulseRise should have no children', () => {
-        const { container } = render(<BallPulseRise loading={false}/>);
+        const { container } = render(<BallPulseRise loading={false} />);
 
         expect(container.firstChild).toBeNull();
     });
 
     test('BallPulseRise should have three children', () => {
-        const { container } = render(<BallPulseRise loading={true}/>);
+        const { container } = render(<BallPulseRise loading={true} />);
 
         expect(container.querySelectorAll('div')).toHaveLength(6);
     });
